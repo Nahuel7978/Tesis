105048a8d7e8b43d9f3235ea3c4003fa4c3747c2
Nahuel7978 <roman.n7978@gmail.com>
2025-05-11 11:08:44 -0300

    Se cambia la deteccion de estados(deteccion de estados_2) y se actualiza el visualizador de politicas

--- Autonomous Robot/controllers/rosbot/Qlearning/ActionAdaptativeHROSbot.py
+++ Autonomous Robot/controllers/rosbot/Qlearning/ActionAdaptativeHROSbot.py
@@ -5,9 +5,12 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
 
     def __init__(self, bot, l_rate, t_descuento, r_exploracion):
         super().__init__(bot, l_rate, t_descuento, r_exploracion)
-        self.putCantidadAcciones(15)
-        self.putCantidadEstados((58*15)) 
+        self.putCantidadAcciones(13)
+        self.candidaAmbientes = 32
+        self.putCantidadEstados((self.candidaAmbientes*self.getCantidadAcciones())) 
+        self.ambienteActual = 0
         self.inicializarPoliticas()
+        
 
     def ejecutar(self, accion):
         ejecucion = False
@@ -29,22 +32,17 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
         elif(accion == 5):
             print("---> giroParaleloObstaculo")
             ejecucion = self.giroParaleloObstaculo()
+
         elif(accion == 6):
-            print("---> giroIzquierdaParaleloObstaculo")
-            ejecucion = self.giroIzquierdaParaleloObstaculo()
-        elif(accion == 7):
-            print("---> giroDerechaParaleloObstaculo")
-            ejecucion = self.giroDerechaParaleloObstaculo()
-        elif(accion == 8):
             print("---> avanzarObstaculo")
             ejecucion = self.avanzarObstaculo()
-        elif(accion == 9):
+        elif(accion == 7):
             print("---> giroAleatorioIzquierda")
             ejecucion = self.giroAleatorioIzquierda()
-        elif(accion == 10):
+        elif(accion == 8):
             print("---> Giro Aleatorio Derecha")
             ejecucion = self.giroAleatorioDerecha()
-        elif(accion == 11):
+        elif(accion == 9):
             print("---> Avanzar Mucho")
             distanciaActual = self.distanciaRecorrida
             ejecucion = self.avanzar(self.distAvanceMax, self.speed) ##Avanzar mucho
@@ -54,7 +52,7 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
             else:
                 print(self.distanciaRecorrida,"> (", distanciaActual,"+",self.distAvanceMedio,")")
 
-        elif(accion == 12):
+        elif(accion == 10):
             print("---> Avanzar Medio")
             distanciaActual = self.distanciaRecorrida
             ejecucion = self.avanzar(self.distAvanceMedio, self.speed) ##Avanzar medio
@@ -64,10 +62,10 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
             else:
                 print(self.distanciaRecorrida,"> (", distanciaActual,"+",self.distAvanceMin,")")
 
-        elif(accion == 13):
+        elif(accion == 11):
             print("---> Avanzar Poco")
             ejecucion = self.avanzar(self.distAvanceMin, self.speed) ##Avanzar
-        elif(accion == 14):
+        elif(accion == 12):
             print("----> Retroceder Poco")
             ejecucion = self.retroceder(0.4, self.speed) ##Retroceder
         else:
@@ -76,9 +74,26 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
         return ejecucion
         
 
-    def estadoActual_2(self, accion):
+    def estadoActual_2(self,accion):
+        """_summary_
+
+        Args:
+            accion (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        amb = self.deteccionAmbiente()
+        return amb+(self.candidaAmbientes*(accion-1))
+
+    def deteccionAmbiente(self):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
         indice = 0
-        dif_sensores = 0.03
+        dif_sensores = 0.02
         min_dif = 0.06
         dist_min = 0.15
         self.robot.step(self.robotTimestep)
@@ -192,12 +207,11 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
                     else: ## Vía Libre
                         indice = 31
 
-        print("Estado Actual:", indice, "Accion anterior: ", accion)
-        print(" ")
-        if(accion == 0):
-            return indice
-        else:
-            return indice+(32*(accion-1))
+        print("Ambiente Actual:", indice)
+        
+        self.ambienteActual = indice
+
+        return indice
 
     def estadoActual(self, accion):
         indice = 0
@@ -425,9 +439,12 @@ class ActionAdaptativeHROSbot(AdaptiveHROSbot):
             Visualiza la tabla de politicas.
         """
         filas = ['Avanzar Senial','Giro Senial','Avanzar Ultima Senial', 'Avanzar Paralelo Obstaculo', 'Giro Paralelo Obstaculo Guiado', 'Giro Paralelo Obstaculo',
-                 'Giro Izquierda Paralelo Obstaculo', 'Giro Derecha Paralelo Obstaculo','Avanzar Obstaculo', 'Giro Aleatorio Izquierda', 
+                 'Avanzar Obstaculo', 'Giro Aleatorio Izquierda', 
                  'Giro Aleatorio Derecha', 'Avanzar Mucho', 'Avanzar Medio', 'Avanzar Poco','Retroceder']
         columnas = [f"S{i}" for i in range(self.cantidadEstados)]
         politicas = pd.DataFrame(self.qLearning, index=filas, columns=columnas)
         print("Q-Learning")
         print(politicas)
+
+    def get_ambienteActual(self):
+        return self.ambienteActual
