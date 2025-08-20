4abe54500ecddd2c421b19ffac3676a01e7dd058
Nahuel7978 <roman.n7978@gmail.com>
2025-08-20 10:08:47 -0300

    Clase de configuracion. Contiene todos paths relevantes

---
+++ SimulationControlApi/Services/core/config.py
@@ -0,0 +1,18 @@
+import os
+class Config:
+    def __init__(self):
+        self.storage_path = os.getenv("STORAGE_PATH", "../Storage/Jobs")
+        self.internal_controller_path = os.getenv("STORAGE_PATH", "../Storage/InternalController")
+
+    def get_storage_path(self):
+        return self.storage_path
+    
+    def get_internal_controller_path(self):
+        return self.internal_controller_path
+    
+    def set_storage_path(self, path):
+        self.storage_path = path
+    
+    def set_internal_controller_path(self, path):
+        self.internal_controller_path = path
+    
