56f837b7289dc3b33360ec3c586a5b6cf87692ae
Nahuel7978 <roman.n7978@gmail.com>
2025-08-25 10:54:19 -0300

    Script para iniciar API

---
+++ SimulationControlApi/start_api.py
@@ -0,0 +1,19 @@
+import uvicorn
+from pathlib import Path
+
+if __name__ == "__main__":
+    current_dir = Path(__file__).parent
+    uvicorn.run(
+            "main:app",
+            host="127.0.0.1",
+            port=8000,
+            reload=True,
+            reload_excludes=[
+                str(current_dir / "Storage"),
+                str(current_dir / "logs"),
+                str(current_dir / "enviroment_api"),
+                "**/__pycache__",
+                "**/*.pyc",
+                "**/*.log"
+            ]
+        )
