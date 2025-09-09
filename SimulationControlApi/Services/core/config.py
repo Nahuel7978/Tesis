693d0680603a70c413aa3291fef0a35b3997a459
Nahuel7978 <roman.n7978@gmail.com>
2025-09-09 11:06:40 -0300

    Se agregan los TTL de los jobs, junto a sus getters y setters

--- SimulationControlApi/Services/core/config.py
+++ SimulationControlApi/Services/core/config.py
@@ -1,18 +1,26 @@
 import os
 class Config:
     def __init__(self):
-        self.storage_path = os.getenv("STORAGE_PATH", "./Storage/Jobs")
-        self.internal_controller_path = os.getenv("STORAGE_PATH", "./Storage/InternalController")
+        self.__storage_path = os.getenv("STORAGE_PATH", "./Storage/Jobs")
+        self.__internal_controller_path = os.getenv("STORAGE_PATH", "./Storage/InternalController")
+        self.__ttlConfig = {
+            "completed_jobs_hours": 24 * 7,  # 7 días para jobs completados exitosamente
+            "failed_jobs_hours": 24 * 1,     # 1 día para jobs fallidos
+        }
+        
 
     def get_storage_path(self):
-        return self.storage_path
+        return self.__storage_path
     
     def get_internal_controller_path(self):
-        return self.internal_controller_path
+        return self.__internal_controller_path
     
     def set_storage_path(self, path):
-        self.storage_path = path
+        self.__storage_path = path
     
     def set_internal_controller_path(self, path):
-        self.internal_controller_path = path
+        self.__internal_controller_path = path
+    
+    def get_ttl_config(self):
+        return self.__ttlConfig
     
