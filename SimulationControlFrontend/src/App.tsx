a8142bc90e7836fb843ada886acea195919cfa10
Nahuel7978 <roman.n7978@gmail.com>
2025-10-04 11:02:30 -0300

    Se agrega los route a cada pagina

--- SimulationControlFrontend/src/App.tsx
+++ SimulationControlFrontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import TestPage from './pages/TestPage';
+import TrainingConfigPage from './pages/TrainingConfigPage';
 import './App.css'
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/training/new" element={<TrainingConfigPage />} />
         <Route path="/test" element={<TestPage />} />
-        <Route path="*" element={<Navigate to="/test" replace />} />
+        {/*<Route path="*" element={<Navigate to="/test" replace />} />*/}
+        <Route path="*" element={<Navigate to="/training/new" replace />} />
       </Routes>
     </BrowserRouter>
   )
