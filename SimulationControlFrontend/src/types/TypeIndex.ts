d934bbf6e9004baf70fe2dbc3db417a08525fd23
Nahuel7978 <roman.n7978@gmail.com>
2025-10-04 10:57:29 -0300

    Se quita la exportación de JobState como type

--- SimulationControlFrontend/src/types/TypeIndex.ts
+++ SimulationControlFrontend/src/types/TypeIndex.ts
@@ -12,5 +12,4 @@ export * from './api';
 
 // Re-export commonly used type combinations
 export type { Job, TrainingMetrics } from './job';
-export type {JobState } from './base';
 export type { TrainingHyperparameters, ModelParams } from './training';
