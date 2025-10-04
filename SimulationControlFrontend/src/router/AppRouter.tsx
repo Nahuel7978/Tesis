1f0859921b1508ce214fc19b35e94316222fedb0
Nahuel7978 <roman.n7978@gmail.com>
2025-10-04 11:02:13 -0300

    Maneja las direcciones de cada pagina

---
+++ SimulationControlFrontend/src/router/AppRouter.tsx
@@ -0,0 +1,5 @@
+// src/router/AppRouter.tsx
+import TrainingConfigPage from '../pages/TrainingConfigPage';
+import {Route} from 'react-router-dom';
+
+<Route path="/training/new" element={<TrainingConfigPage />} />
