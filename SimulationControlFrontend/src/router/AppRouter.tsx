405da6e8881bba5769af1e49539e3727da013164
Nahuel7978 <roman.n7978@gmail.com>
2025-10-08 09:07:05 -0300

    Se agruegan las rutas de las paginas: - Dashboard - TrainConfigPage - TestPage - MainLayout

--- SimulationControlFrontend/src/router/AppRouter.tsx
+++ SimulationControlFrontend/src/router/AppRouter.tsx
@@ -1,5 +1,36 @@
 // src/router/AppRouter.tsx
-import TrainingConfigPage from '../pages/TrainingConfigPage';
-import {Route} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { MainLayout } from '@/components/layout/MainLayout';
+import { Dashboard } from '../pages/DashboardPage';
+import  TrainingConfigPage from '../pages/TrainingConfigPage';
+import TestPage from '@/pages/TestPage';
+// Importa aquí tu TrainPage cuando la tengas
+// import { TrainPage } from '@/features/training/pages/TrainPage';
 
-<Route path="/training/new" element={<TrainingConfigPage />} />
+export const AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <MainLayout>
+        <Routes>
+          {/* Redirigir la raíz al dashboard */}
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          
+          {/* Dashboard */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          
+          {/* Cargar Mundo */}
+          <Route path="/training/new" element={<TrainingConfigPage />} />
+          
+          <Route path="/test" element={<TestPage/>} />
+
+          {/* Detalle de Training (cuando lo implementes) */}
+          {/* <Route path="/train/:jobId" element={<TrainPage />} /> */}
+          
+          {/* 404 - Ruta no encontrada */}
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  );
+};
