8aeeaba1d9049a4fd23cc34ea864d085538f0a35
Nahuel7978 <roman.n7978@gmail.com>
2025-10-08 08:57:58 -0300

    Se quita la lógica de ruteo y solo se muestra AppRouter

--- SimulationControlFrontend/src/App.tsx
+++ SimulationControlFrontend/src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import TestPage from './pages/TestPage';
-import TrainingConfigPage from './pages/TrainingConfigPage';
 import './App.css'
+import { AppRouter } from './router/AppRouter';
 
 function App() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/training/new" element={<TrainingConfigPage />} />
-        <Route path="/test" element={<TestPage />} />
-        {/*<Route path="*" element={<Navigate to="/test" replace />} />*/}
-        <Route path="*" element={<Navigate to="/training/new" replace />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return <AppRouter />
 }
 
 export default App
