54ff49d2f4a9acde8a7151e7327906bcb863e153
Nahuel7978 <roman.n7978@gmail.com>
2025-10-08 08:59:57 -0300

    Capa principal. Siempre se mostrará en todas las páginas. En este caso especifico el sidebar

---
+++ SimulationControlFrontend/src/components/layout/MainLayout.tsx
@@ -0,0 +1,18 @@
+// src/components/layout/MainLayout.tsx
+import React from 'react';
+import { Sidebar } from './Sidebar';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 ml-64">
+        {children}
+      </main>
+    </div>
+  );
+};
